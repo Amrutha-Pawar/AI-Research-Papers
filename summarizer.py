e73d0a069e6a0dd290898d8cb3b5a93b0971c0b6
AmruthaPawar <amruthapawar72@gmail.com>
2026-05-29 14:37:47 +0530

    Fix API issue

--- summarizer.py
+++ summarizer.py
@@ -1,9 +1,12 @@
-from groq import Groq
-import os
 from dotenv import load_dotenv
-
 load_dotenv()
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+import os
+from groq import Groq
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
 def summarize_text(text):
 
